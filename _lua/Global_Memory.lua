6a469642ade9ef7c1e0d6926d9d137db78474eda
Jan Schmitte <janschmitte@gmail.com>
2021-02-21 18:02:33 +0100

    Global_Memory.lua * corrected Y- to Z-axis * made the tiles spawn in a loop

--- _lua/Global_Memory.lua
+++ _lua/Global_Memory.lua
@@ -1,23 +1,36 @@
-function Memory_spawnTiles(numX, numY)
-    log('spawn begin')
+function Memory_spawnTiles(numX, numZ)
+    -- set the upper left point where the tiles will be placed
     startPosX = 0
-    startPosY = 0
+    startPosZ = 0
 
-    spawnParams = {
-        type = 'Custom_Tile',
-        position = {x = 0, y = 0, z = 0},
-        rotation = {x = 0, y = 0, z = 0},
-        scale = {x = 1, y = 1, z = 1},
-        sound = false,
-        snap_to_grid = true
-    }
-    newObj = spawnObject(spawnParams)
+    -- set the offset between tiles
+    offsetX = 3
+    offsetZ = 3
 
-    customParams = {
-        image = songpack.settings.icon,
-        type = 3,
-        thickness = 0.1,
-        stackable = false
-    }
-    newObj.setCustomObject(customParams)
+    -- for each tile horizontally...
+    for x = 0, numX - 1, 1 do
+        -- for each tile vertically...
+        for z = 0, numZ - 1, 1 do
+            -- compute the new Position by startPos +- (index * offset)
+            newPos = {x = startPosX + (x * offsetX), y = 1, z = startPosZ - (z * offsetZ)}
+
+            spawnParams = {
+                type = 'Custom_Tile',
+                position = newPos,
+                rotation = {x = 0, y = 180, z = 0},
+                scale = {x = 1, y = 1, z = 1},
+                sound = false,
+                snap_to_grid = true
+            }
+            newObj = spawnObject(spawnParams)
+        
+            customParams = {
+                image = songpack.settings.icon,
+                type = 3,
+                thickness = 0.1,
+                stackable = false
+            }
+            newObj.setCustomObject(customParams)
+        end
+    end 
 end
