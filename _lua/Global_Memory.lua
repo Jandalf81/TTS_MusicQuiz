3049dc9ddedc896518529b7ab4307fbcabf68fda
Jan Schmitte <janschmitte@gmail.com>
2021-02-21 19:12:56 +0100

    Global_Memory.lua * extended function "Memory_spawnTiles(numX, numZ)"    * add button to each tile    * add GMNotes to each tile * added function "Memory_playSong(obj)"    * saves the guesses    * plays the song of the current guess    * if it's the second guess compare them    * reset both guesses if not identical

--- _lua/Global_Memory.lua
+++ _lua/Global_Memory.lua
@@ -7,6 +7,8 @@ function Memory_spawnTiles(numX, numZ)
     offsetX = 3
     offsetZ = 3
 
+    math.randomseed(os.time())
+
     -- for each tile horizontally...
     for x = 0, numX - 1, 1 do
         -- for each tile vertically...
@@ -14,6 +16,7 @@ function Memory_spawnTiles(numX, numZ)
             -- compute the new Position by startPos +- (index * offset)
             newPos = {x = startPosX + (x * offsetX), y = 1, z = startPosZ - (z * offsetZ)}
 
+            -- set the spawn parameters and spawn the tile
             spawnParams = {
                 type = 'Custom_Tile',
                 position = newPos,
@@ -24,6 +27,7 @@ function Memory_spawnTiles(numX, numZ)
             }
             newObj = spawnObject(spawnParams)
         
+            -- set the custom parameters needed for a tile
             customParams = {
                 image = songpack.settings.icon,
                 type = 3,
@@ -31,6 +35,55 @@ function Memory_spawnTiles(numX, numZ)
                 stackable = false
             }
             newObj.setCustomObject(customParams)
+
+            -- attach a button to each tile
+            btn_Play = {
+                index = 0,
+                click_function = 'Memory_playSong',
+                function_owner = Global,
+                label = 'Play',
+                position = {0, 0.11, 0},
+                rotation = {0, 0, 0},
+                width = 400,
+                height = 400,
+                font_size = 150
+            }
+            newObj.createButton(btn_Play)
+
+            -- TODO: Insert a random number of 1 to (#songpack.memory.pool) here. Each number must be given exactly twice
+            newObj.setGMNotes(math.random(1, 3))
         end
     end 
+end
+
+function Memory_playSong(obj)
+    -- play the song with the objects associated number from songpack.memory.pool
+    playSong(songpack.memory.pool[tonumber(obj.getGMNotes())])
+
+    -- change the button to orange
+    obj.editButton({index = 0, color = {1, 1, 0}})
+
+    -- save the guesses
+    if (Memory_firstGuess == nil) then
+        -- first guess
+        Memory_firstGuess = obj
+    else
+        -- second guess
+        Memory_secondGuess = obj
+
+        -- compare the guesses
+        if (Memory_firstGuess.getGMNotes() == Memory_secondGuess.getGMNotes()) then
+           log('Treffer') 
+           Memory_firstGuess.editButton({index = 0, color = {0, 1, 0}})
+           Memory_secondGuess.editButton({index = 0, color = {0, 1, 0}})
+        else
+            log('Daneben')
+            Memory_firstGuess.editButton({index = 0, color = {1, 1, 1}})
+            Memory_secondGuess.editButton({index = 0, color = {1, 1, 1}})
+        end
+
+        -- reset the guesses
+        Memory_firstGuess = nil
+        Memory_secondGuess = nil
+    end
 end
